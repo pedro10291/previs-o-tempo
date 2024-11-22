578188e2aaf37a574d3b637efcb07c23a721810f
Pedro Coutinho <121466312+pedro10291@users.noreply.github.com>
2024-11-22 14:09:56 -0300

    Update script.js

--- previsão do tempo/script.js
+++ previsão do tempo/script.js
@@ -9,22 +9,27 @@ function DadosNaTela(dados) {
     // ... outras linhas para atualizar a interface
 }
 
+
 async function buscarCidade(cidade) {
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json());
+    try {
+        const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json());
 
-    if (dados.name) {
-        DadosNaTela(dados);
-    } else {
-        const cidadeDigitada = document.querySelector(".cidade").value;
-        document.querySelector(".altera").innerHTML = `Não foi possível encontrar a cidade`;
-
-        // Limpa os outros elementos da interface
-        document.querySelector(".altera").innerHTML = `Tempo em ${dados.name} ${Math.floor(dados.main.temp)}°C`;
-        document.querySelector(".tempmin").innerHTML = "Min " + Math.floor(dados.main.temp_min) + "°C";
-        document.querySelector(".tempmax").innerHTML = "Max " + Math.floor(dados.main.temp_max) + "°C";
-        document.querySelector(".txt-previsao").innerHTML = dados.weather[0].description;
-        document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + "%";
-        document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+        if (dados.name) {
+            // Atualizar a interface com todos os dados
+                document.querySelector(".altera").innerHTML = `Tempo em ${dados.name} ${Math.floor(dados.main.temp)}°C`;
+            document.querySelector(".tempmin").innerHTML = "Min " + Math.floor(dados.main.temp_min) + "°C";
+            document.querySelector(".tempmax").innerHTML = "Max " + Math.floor(dados.main.temp_max) + "°C";
+            document.querySelector(".txt-previsao").innerHTML = dados.weather[0].description;
+            document.querySelector(".umidade").innerHTML = "Umidade " + dados.main.humidity + "%";
+            document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+        
+            // ... outras atualizações
+        } else {
+            document.querySelector(".altera").innerHTML = `Cidade não encontrada.`;
+        }
+    } catch (error) {
+        console.error("Erro ao buscar a cidade:", error);
+        document.querySelector(".altera").innerHTML = "Ocorreu um erro ao buscar a cidade.";
     }
 }
 
@@ -33,3 +38,4 @@ function lupa() {
     buscarCidade(cidade);
     document.querySelector('.cidade').value = "";
 }
+
